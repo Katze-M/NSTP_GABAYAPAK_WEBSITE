684ec3691e1dc1d32396b38015db389f2536669a
Katze-M <katze.1leisure@gmail.com>
2026-07-15 16:04:38 +0800

    feat: complete project controller and model structural modifications

--- nstp-gabayapak-website/app/Models/Project.php
+++ nstp-gabayapak-website/app/Models/Project.php
@@ -59,6 +59,8 @@ class Project extends Model
     protected $casts = [
         'student_ids' => 'array',
         'member_roles' => 'array',
+        'is_resubmission' => 'boolean',
+        'resubmission_count' => 'integer',
     ];
 
     /**
